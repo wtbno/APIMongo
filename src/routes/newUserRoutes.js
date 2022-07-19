33f1a49b3b2e27459acaad378c1aaf6e4ef3475a
wtbno <bruno_links@hotmail.com>
2022-07-19 19:21:56 -0300

    feat.: conclusão do crud

--- src/routes/newUserRoutes.js
+++ src/routes/newUserRoutes.js
@@ -15,7 +15,7 @@ router.post("/", async (req, res) => {
 
   if ((!name, !email, !password, !birthDate)) {
     res.status(422).json({ error: "Todos os campos são obrigatórios!" });
-    return
+    return;
   }
 
   try {
@@ -38,19 +38,63 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-
-  const id = req.params.id
+  const id = req.params.id;
   try {
     //mongodb utiliza _id
-    const newUser = await NewUser.findOne({_id: id});
+    const newUser = await NewUser.findOne({ _id: id });
 
-
-    if(!newUser){
-      res.status(422).json({message:'Usuário não encontrado'})
-      return
+    if (!newUser) {
+      res.status(422).json({ message: "Usuário não encontrado" });
+      return;
     }
 
     res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "internal error" });
+  }
+});
+
+//Update (put,patch)
+//put objeto completo
+//patch atualização parcial
+
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const { name, email, password, birthDate } = req.body;
+
+  const newUser = {
+    name,
+    email,
+    password,
+    birthDate,
+  };
+
+  try {
+    const updatedNewUser = await NewUser.updateOne({ _id: id }, newUser);
+    console.log(updatedNewUser);
+
+    if (updatedNewUser.matchedCount === 0) {
+      res.status(422).json({ message: "Usuário não encontrado" });
+    }
+
+    res.status(200);
+  } catch (error) {
+    res.status(500).json({ error: "internal error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const newUser = await NewUser.findOne({ _id: id });
+  if (!newUser) {
+    res.status(422).json({ message: "Usuário não encontrado" });
+    return;
+  }
+
+  try {
+    await NewUser.deleteOne({ _id: id });
+    res.status(200).json({message: 'Usuário removido com sucesso!'})
   } catch (error) {
     res.status(500).json({ error: "internal error" });
   }
